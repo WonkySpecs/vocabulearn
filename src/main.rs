a387d500baf6777d0f89bac599d418b4f12195a5
Will Taylor <w1ll100@hotmail.co.uk>
2019-10-21 20:16:40 +0100

    Initial data structures

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,40 @@ use std::io;
 use chrono::{ DateTime, Utc };
 use serde::Deserialize;
 
+const VOCAB_FILE: &str = "vocab.csv";
+const LABELS_FILE: &str = "labels.csv";
+const LABEL_MAP_FILE: &str = "label_map.csv";
+
 #[derive(Deserialize, Debug)]
 struct VocabItem {
-    native: String,
+    id: usize,
+    in_native_lang: String,
     transliterated: Option<String>,
-    original_language: Option<String>,
+    in_original_lang: Option<String>,
     time_added: DateTime<Utc>,
 }
 
+
+#[derive(Deserialize, Debug)]
+enum LabelType {
+    WordType, WordArea, Group,
+}
+
+#[derive(Deserialize, Debug)]
+struct Label {
+    id: usize,
+    display_name: String,
+    label_type: LabelType,
+}
+
+#[derive(Deserialize, Debug)]
+struct ItemLabel {
+    item_id: usize,
+    label_id: usize,
+}
+
 fn read_vocab() -> Result<(), Box<dyn Error>> { 
-    let mut rdr = csv::Reader::from_path("test.csv")?;
+    let mut rdr = csv::Reader::from_path(VOCAB_FILE)?;
     for result in rdr.deserialize() {
         let item: VocabItem = result?;
         println!("{:?}", item);
